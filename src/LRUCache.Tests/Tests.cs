5b199f0233ac7a71d6aad4645b6e1e6761abd36e
Tom Jacques <tejacques@gmail.com>
2014-10-29 17:19:20 -0400

    Fix folder structure

---
+++ src/LRUCache.Tests/Tests.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LRUCache.Tests
+{
+    public class Tests
+    {
+    }
+}
